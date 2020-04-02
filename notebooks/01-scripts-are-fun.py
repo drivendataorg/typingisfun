8dbe70d9c18ae76acb2898e9c20c9ef7a301f74c
Robert Gibboni <robert@drivendata.org>
2020-04-02 16:00:51 -0700

    Remove unneeded line

--- notebooks/01-scripts-are-fun.py
+++ notebooks/01-scripts-are-fun.py
@@ -1,6 +1,5 @@
 from typingisfun.v1 import lets_add_these, lets_add_those, now_were_really_adding
 
-" ".join([1, 2,3 ])
 lets_add_these(1, 1)
 lets_add_those("we're ", "very ")
 now_were_really_adding(1, 1)
